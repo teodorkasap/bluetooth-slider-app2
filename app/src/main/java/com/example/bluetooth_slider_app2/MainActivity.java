2350bcc39441a1e8eaeb08361861fca5cb41fb4b
teodorkasap <erol.erten@gmail.com>
2020-05-22 22:26:31 +0300

    slider listener added

--- app/src/main/java/com/example/bluetooth_slider_app2/MainActivity.java
+++ app/src/main/java/com/example/bluetooth_slider_app2/MainActivity.java
@@ -2,13 +2,48 @@ package com.example.bluetooth_slider_app2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SeekBar simpleSeekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        simpleSeekBar = findViewById(R.id.seekBar);
+
+        simpleSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progressChangedValue = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                progressChangedValue = progress;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+                Toast.makeText(MainActivity.this, "Seek bar progress is :" + progressChangedValue,
+                        Toast.LENGTH_SHORT).show();
+
+//                valString = String.format("%d\n", progressChangedValue);
+//                byte[] valBytes = val.getBytes();
+
+            }
+
+
+        });
     }
 }
