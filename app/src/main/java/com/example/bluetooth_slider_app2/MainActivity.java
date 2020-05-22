c9fef4ff8e981e2b30ac2942240af2787148e4ea
teodorkasap <erol.erten@gmail.com>
2020-05-22 23:39:29 +0300

    bluetooth search button onClick method intendfilter

--- app/src/main/java/com/example/bluetooth_slider_app2/MainActivity.java
+++ app/src/main/java/com/example/bluetooth_slider_app2/MainActivity.java
@@ -2,21 +2,65 @@ package com.example.bluetooth_slider_app2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.SeekBar;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private SeekBar simpleSeekBar;
+    private ListView listView;
+    private Button searchButton;
+    private BluetoothAdapter bluetoothAdapter;
+
+    private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            Log.i("Action",action);
+
+            if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+
+                Toast.makeText(MainActivity.this, "Bluetooth aygıtları taraması bitti...",
+                        Toast.LENGTH_SHORT).show();
+                searchButton.setEnabled(true);
+            }
+        }
+    };
+
+    public void searchButtonClick(View view){
+        Toast.makeText(MainActivity.this, "Bluetooth aygıtları taranıyor...",
+                Toast.LENGTH_SHORT).show();
+        searchButton.setEnabled(false);
+        bluetoothAdapter.startDiscovery();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         simpleSeekBar = findViewById(R.id.seekBar);
+        searchButton = findViewById(R.id.searchButton);
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
+        intentFilter.addAction(BluetoothDevice.ACTION_FOUND);
+        intentFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+        intentFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        registerReceiver(broadcastReceiver,intentFilter);
+
+
 
         simpleSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             int progressChangedValue = 0;
@@ -45,5 +89,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         });
+
+
     }
 }
